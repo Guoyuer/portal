25320c9bf32d4820415bc72ef2d66e0c8f0a0524
Yuer Guo <guoyuer1@gmail.com>
2026-04-04 15:54:55 -0400

    ci: Force 4 Playwright workers in CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   testDir: "./e2e",
   fullyParallel: true,
   retries: 0,
+  workers: process.env.CI ? 4 : undefined,
   use: {
     baseURL: "http://localhost:3000",
   },
